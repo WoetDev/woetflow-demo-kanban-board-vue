60e00722998d4e811c96e84cb92e52597a99a647
SprouterB <wouter.bruynsteen@gmail.com>
2021-01-31 23:56:58 +0100

    Add footer + _redirects

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,21 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hhh Lpr fff">
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer>
+      <div class="text-center q-pa-md">
+        <div>
+          Demo full-stack Kanban Board by
+          <a href="https://woetflow.com/" target="_blank">Woet Flow</a>
+        </div>
+        <div>
+          Github: 
+          <a href="https://github.com/WoetDev/woetflow-demo-kanban-board-vue" target="_blank">Vue SPA Frontend</a> / 
+          <a href="https://github.com/WoetDev/woetflow-demo-kanban-board-api" target="_blank">Ruby on Rails API Backend</a>
+        </div>
+      </div>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -11,3 +24,13 @@ export default {
   name: 'MainLayout'
 }
 </script>
+
+<style scoped lang="scss">
+
+footer {
+  a {
+    color: white;
+    font-weight: bold;
+  }
+}
+</style>
