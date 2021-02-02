1a7de018955d4b91532ab3762576a649445a7ed1
SprouterB <wouter.bruynsteen@gmail.com>
2021-02-02 22:22:01 +0100

    Update README

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,23 @@
 <template>
   <q-page>
-    <div class="flex justify-center">
-      <div class="text-h4 text-primary q-py-md"><b>Kanban board</b></div>
-    </div>
+      <div class="flex justify-center">
+        <div class="text-h4 text-primary q-py-md"><b>Kanban board</b></div>
+      </div>
 
-    <div class="flex justify-center">
-      <ManageCard />
-    </div>
+      <div class="flex justify-center">
+        <ManageCard />
+      </div>
 
-    <div class="flex justify-center">
-      <Column v-for="column in columns" :key="column.id" :column="column"  />
-    </div>
+      <div v-if="isLoading" class="flex justify-center q-pt-xl">
+        <q-spinner
+          color="primary"
+          size="3em"
+        />
+      </div>
+
+      <div v-else class="flex justify-center">
+        <Column v-for="column in columns" :key="column.id" :column="column"  />
+      </div>
   </q-page>
 </template>
 
@@ -21,6 +28,9 @@ import ManageCard from '../components/ManageCard'
 
 export default {
   name: 'PageIndex',
+  data: () => ({
+    isLoading: true
+  }),
   components: {
     Column,
     ManageCard
@@ -35,6 +45,7 @@ export default {
   created() {
     api.getColumns()
       .then(response => {
+        this.isLoading = false
         this.$store.commit("setColumns", response.data)
       })
       .catch(error => console.log(error))
